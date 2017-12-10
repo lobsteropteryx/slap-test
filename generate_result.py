a42548563681b9015a26d18e571f11cc5f5ae5b1
ifirkin <ian.firkin@homeinsteadinc.com>
2017-12-10 12:06:41 -0500

    Fixes fgdb path

--- generate_result.py
+++ generate_result.py
@@ -2,7 +2,7 @@ import arcpy
 import os
 
 arcpy.ImportToolbox('test.pyt')
-dataset = os.path.normpath(os.path.join(os.getcwd(), 'slap_test.gdb'))
+dataset = os.path.normpath(os.path.join(os.getcwd(), 'slap-test.gdb'))
 result = arcpy.slaptest.SlapTest(os.path.join(dataset, 'points'))
 result.saveToFile('test.rlt')
 
