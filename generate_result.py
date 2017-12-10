2108eedd71719283895694b2ab8d9264075b7170
ifirkin <ian.firkin@homeinsteadinc.com>
2017-12-10 10:15:54 -0500

    Get full path to dataset

--- generate_result.py
+++ generate_result.py
@@ -1,6 +1,8 @@
 import arcpy
+import os
 
 arcpy.ImportToolbox('test.pyt')
+dataset = os.path.normpath(os.path.join(os.getcwd(), 'slap_test.gdb')
 result = arcpy.slaptest.SlapTest('slap_test.gdb/points')
 result.saveToFile('test.rlt')
 
