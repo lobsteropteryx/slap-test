17d6e72f7d7a62775a8eb5f50ac718706619ce19
ifirkin <ian.firkin@homeinsteadinc.com>
2017-12-10 20:26:40 -0500

    Removes unused script

--- generate-result.py
+++
@@ -1,8 +0,0 @@
-import arcpy
-import os
-
-arcpy.ImportToolbox('test.pyt')
-dataset = os.path.normpath(os.path.join(os.getcwd(), 'slap-test.gdb'))
-result = arcpy.slaptest.SlapTest(os.path.join(dataset, 'points'))
-result.saveToFile('test.rlt')
-
